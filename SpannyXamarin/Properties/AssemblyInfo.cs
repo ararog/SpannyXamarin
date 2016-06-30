58137fcedc6b815c7b04eb71609d2eede25fb540
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2016-06-30 19:42:55 -0300

    Updating assembly info.

--- SpannyXamarin/Properties/AssemblyInfo.cs
+++ SpannyXamarin/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpannyXamarin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Xamarin port of the Android Spanny library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpannyXamarin")]
